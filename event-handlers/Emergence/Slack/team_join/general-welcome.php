af4b6e7df64a9bac58fdff20d28aa5a9a4f8b71a
Miroslav <schlos@users.noreply.github.com>
2020-11-23 10:10:03 +0100

    Disabling General Slack welcome message
    Fixes #80

--- event-handlers/Emergence/Slack/team_join/general-welcome.php
+++ event-handlers/Emergence/Slack/team_join/general-welcome.php
@@ -1,8 +1,8 @@
 <?php
 
-Emergence\Slack\API::request('chat.postMessage', [
-    'post' => [
-        'channel' => Emergence\Slack\API::getChannelId('general'),
-        'text' => ":waving: Welcome, <@{$_EVENT['user']['id']}>! What are you hoping to get out of your time with Code for Croatia?"
-    ]
-]);
+#Emergence\Slack\API::request('chat.postMessage', [
+#    'post' => [
+#        'channel' => Emergence\Slack\API::getChannelId('general'),
+#        'text' => ":wave: Pozdrav, <@{$_EVENT['user']['id']}>! Reci nam nešto o sebi na kanalu #introductions. Što očekuješ od Code for Croatia? U Zagrebu djeluje naša lokalna grupa Open Zagreb koja se povremeno sastaje - pronađi informaciju o nadolazećim meetupima na kanalu #openzagreb. Želiš li osnovati lokalnu grupu u svom gradu po uzoru modela Code for Croatia? Podržavamo kreiranje lokalnih civic hacking zajednica - javi nam se za detalje. Pridruži se kanalu #help-wanted ako mislis da nam možeš pomoći."
+#    ]
+#]);
